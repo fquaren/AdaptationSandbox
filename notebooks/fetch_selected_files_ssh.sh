8c4c70e4777fe4fffac2d98b1056ce677b2b2004
Filippo Quarenghi <filippo.quarenghi@gmail.com>
2025-03-21 14:18:43 +0100

    EOF bug

--- notebooks/fetch_selected_files_ssh.sh
+++ notebooks/fetch_selected_files_ssh.sh
@@ -27,7 +27,7 @@ LOCAL_PATH="/work/FAC/FGSE/IDYST/tbeucler/downscaling/fquareng/data/DA/T_2M"
 FILE_LIST="/work/FAC/FGSE/IDYST/tbeucler/downscaling/fquareng/data/DA/selected_files.txt"
 
 # Step 1: SSH into the remote server and find the selected files
-ssh -J $REMOTE_USER@$FRONTEND_HOST $REMOTE_USER@$REMOTE_HOST << "EOF" 
+ssh -J $REMOTE_USER@$FRONTEND_HOST $REMOTE_USER@$REMOTE_HOST << EOF 
 cd /capstor/store/cscs/c2sm/scclim/climate_simulations/RUN_2km_cosmo6_climate/output/lm_f/1h_2D
 
 # List all files, filter only those from the year 2011, and sort them
