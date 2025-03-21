dcb73dd5707af7038223a7ab5c7ac838600a80fb
Filippo Quarenghi <filippo.quarenghi@gmail.com>
2025-03-21 14:37:38 +0100

    save output on local file

--- notebooks/fetch_selected_files_ssh.sh
+++ notebooks/fetch_selected_files_ssh.sh
@@ -24,16 +24,15 @@ REMOTE_PATH="/capstor/store/cscs/c2sm/scclim/climate_simulations/RUN_2km_cosmo6_
 LOCAL_PATH="/work/FAC/FGSE/IDYST/tbeucler/downscaling/fquareng/data/DA/T_2M"
 
 # File to store the list of selected files
+# FILE_LIST="/work/FAC/FGSE/IDYST/tbeucler/downscaling/fquareng/data/DA/selected_files.txt"
 FILE_LIST="/work/FAC/FGSE/IDYST/tbeucler/downscaling/fquareng/data/DA/selected_files.txt"
 
 # Step 1: SSH into the remote server and find the selected files
-ssh -J $REMOTE_USER@$FRONTEND_HOST $REMOTE_USER@$REMOTE_HOST <<EOF >> ~/selected_files.txt
-#!/bin/bash
-
+ssh -J $REMOTE_USER@$FRONTEND_HOST $REMOTE_USER@$REMOTE_HOST << 'EOF'
 cd /capstor/store/cscs/c2sm/scclim/climate_simulations/RUN_2km_cosmo6_climate/output/lm_f/1h_2D
 
 # List all files, filter only those from the year 2011, and sort them
-find lffd2011*.nz -type f | sort > ~/all_files.txt
+find lffd2011*.nz -type f | sort > ~/2011_files.txt
 
 # Initialize shift counter
 shift=0
@@ -78,16 +77,16 @@ for date in "${sorted_dates[@]}"; do
 
     # Print selected files (to be captured in selected_files.txt)
     if [[ ${#selected_files[@]} -eq 3 ]]; then
-        printf "%s\n" "${selected_files[@]}"
+        printf "%s\n" "${selected_files[@]}" >> ~/selected_files.txt
     fi
 
     # Increment shift
     shift=$(( (shift + 1) % 24 ))
 done
-EOF
+EOF > $FILE_LIST
 
 # Step 2: Copy the selected files from the remote server
-rsync -avz -e "ssh -J $REMOTE_USER@$FRONTEND_HOST" $REMOTE_USER@$REMOTE_HOST:~/selected_files.txt $FILE_LIST
+# rsync -avz -e "ssh -J $REMOTE_USER@$FRONTEND_HOST" $REMOTE_USER@$REMOTE_HOST:~/selected_files.txt $FILE_LIST
 
 # Step 3: Copy the selected NetCDF files from remote to local
 mkdir -p $LOCAL_PATH
@@ -102,56 +101,3 @@ while read file; do
     # Alternative using NCO:
     # ncks -v T_2M "$file" "$new_file"
 done < $FILE_LIST
-
-
-
-# # List all files, filter only those from the year 2011, and sort them
-# ls lffd2011*.nz | sort > ~/all_files.txt
-
-# # Initialize shift counter
-# shift=0
-
-# # Declare an associative array to group files by date
-# declare -A files_by_date
-
-# # Loop through files and categorize them by date
-# while read file; do
-#     # Extract date and hour from the file name
-#     date_part=$(echo "$file" | grep -oE '2011[0-9]{4}')
-#     hour_part=$(echo "$file" | grep -oE '[0-9]{10}' | cut -c9-10)
-
-#     # Append the hour and filename to the associative array for the corresponding date
-#     files_by_date["$date_part"]+="$hour_part $file "
-# done < ~/all_files.txt
-
-# # Sort dates
-# sorted_dates=(\$(printf "%s\n" "\${!files_by_date[@]}" | sort))
-
-# # Process each day and select the required hours
-# for date in "\${sorted_dates[@]}"; do
-#     # Define the selected hours for the current shift
-#     selected_hours=(\$(( (0 + shift) % 24 )) \$(( (8 + shift) % 24 )) \$(( (15 + shift) % 24 )))
-
-#     # Extract file list for the date
-#     IFS=\$'\n'
-#     files=(\$(echo -e "\${files_by_date[\$date]}" | sort -n))
-
-#     # Select files matching the desired hours
-#     selected_files=()
-#     for entry in "\${files[@]}"; do
-#         read -r hour filename <<< "\$entry"
-#         for h in "\${selected_hours[@]}"; do
-#             if [[ "\$hour" -eq "\$h" ]]; then
-#                 selected_files+=("\$filename")
-#             fi
-#         done
-#     done
-
-#     # Print selected files (to be captured in selected_files.txt)
-#     if [[ "\${#selected_files[@]}" -eq 3 ]]; then
-#         printf "%s\n" "\${selected_files[@]}"
-#     fi
-
-#     # Increment shift
-#     shift=\$(( (shift + 1) % 24 ))
-# done
